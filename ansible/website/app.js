d6ab1710234c02d8f3973d2c1f3137b36a78bfce
vinohts <vino.hts@gmail.com>
2026-07-11 02:18:24 +0530

    Updated Version 3.0

--- ansible/website/app.js
+++ ansible/website/app.js
@@ -1,9 +1,10 @@
+// Set hostname immediately
 document.getElementById("hostname").innerHTML = window.location.hostname;
 
+// Set placeholder statuses
 document.getElementById("instanceid").innerHTML = "Fetching...";
-
 document.getElementById("privateip").innerHTML = "Fetching...";
-
 document.getElementById("az").innerHTML = "Fetching...";
 
+// Localized timestamp
 document.getElementById("time").innerHTML = new Date().toLocaleString();
